505fd707f6a5a7a1e29a5a807c3f128388ad8c61
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-09-08 00:44:10 +0200

    Add fail-fast
    By default app writes {4,5,6}xx errors to stderr
    with `--fail-fast` it throws error

--- lib/Resolver.rb
+++ lib/Resolver.rb
@@ -2,20 +2,37 @@ module Resolver
 
   class Cli
     def main
-      ARGV.each do |i|
-        puts "org: #{i}"
+      opts, args = ARGV.partition {|i| i == "--fail-fast"}
+      failfast = !opts.empty?
+      args.each do |i|
         if i =~ /\A#{URI::regexp(['http', 'https'])}\z/
-          puts Url.new.resolve(i)
+          eitherUrl = Url.new(failfast).resolve(i)
+          if eitherUrl.left?
+            STDERR.puts eitherUrl.value
+          else
+            puts eitherUrl.value
+          end
         else
-          puts "'#{i}' is not a valid url"
+          msg = "'#{i}' is not a valid url"
+          if failfast
+            raise msg
+          else
+            STDERR.puts msg
+          end
         end
       end
     end
-  end # of class
+  end
 
   class Url
     require 'uri'
     require 'net/https'
+
+    attr_reader :failfast
+
+    def initialize(failfast = false)
+        @failfast = failfast
+    end
 
     def resolve url
       resolve_url (normalize_url url)
@@ -28,12 +45,17 @@ module Resolver
 
       case response.code.to_i
       when 400...600
-        raise "server returned #{response.code} #{response.message} for #{url}"
+        msg = "#{response.code} #{response.message} for #{url}"
+        if @failfast
+          raise IOError, msg
+        else
+          Either.left msg
+        end
       when 301
         new_location = normalize_url response['location']
         resolve_url(new_location)
       else
-        url
+        Either.right url
       end
     end
 
@@ -52,5 +74,46 @@ module Resolver
       end
     end
 
-  end # of class
+  end
+
+  class Either
+    class << self
+
+      def left(value)
+        new(value, true).freeze
+      end
+
+      def right(value)
+        new(value, false).freeze
+      end
+    end
+
+    def left
+      left? ? @value : nil
+    end
+
+    def right
+      right? ? @value : nil
+    end
+
+    def left?
+      @is_left
+    end
+
+    def right?
+      ! left?
+    end
+
+    def value
+      @value
+    end
+
+    private
+
+    def initialize(value, is_left)
+      @value = value
+      @is_left = is_left
+    end
+  end
+
 end # of module
